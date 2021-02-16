b679c1538f10d4673129333d8073e1a5f1fafbfd
NikolasAlencar <nikolasalencarrs73@gmail.com>
2021-02-15 23:47:25 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,8 @@ import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
 	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -22,7 +24,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -30,6 +32,10 @@ public class Main extends Application {
 		catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 	
 	public static void main(String[] args) {
